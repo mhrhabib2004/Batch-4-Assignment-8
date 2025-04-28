bc35ec8a15c2d6252edcb9e8d5096519ea01957a
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-04-29 00:19:11 +0600

    add:service route

---
+++ src/app/modules/Service/service.routes.ts
@@ -0,0 +1,48 @@
+
+import express from 'express';
+import { serviceController } from './service.controller';
+import validateRequest from '../../utils/validateRequest';
+import { serviceValidation } from './service.validation';
+
+const router = express.Router();
+
+
+// Create bike route
+router.post(
+    "/create-service-record",
+    validateRequest(serviceValidation.createServiceValidation),
+    serviceController.createService
+);
+
+
+// get all service route
+router.get(
+    '/',
+    serviceController.getAllService
+);
+
+
+// service data befor 7 days and filter panding & in_progress route
+router.get(
+    '/status',
+    serviceController.getPendingOrOverdueServices
+);
+
+
+// get service ID 
+router.get(
+    '/:serviceId',
+    serviceController.getByServiceId
+);
+
+
+// complet service route
+router.put(
+    '/:serviceId/complete',
+    serviceController.completeService
+);
+
+
+
+
+export const ServiceRoutes = router;
